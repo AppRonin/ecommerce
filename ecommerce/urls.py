f5068eb018b972f5c843265ca14ed0f7a85da4bd
Victor <victorfariasbezerra12@gmail.com>
2026-01-24 18:02:42 -0300

    Small Fix

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.home),
     path('store/', include("store.urls"))
 
 ]
