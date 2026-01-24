603d803ca1adad41ec290de47382626e4540cdde
Victor <victorfariasbezerra12@gmail.com>
2026-01-24 13:34:52 -0300

    Store Page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from django.conf import settings
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('store/', include("store.urls"))
+
 ]
 
 # In development (Django serves) in production (Nginx serves)
