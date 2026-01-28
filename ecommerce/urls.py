f0ac02ff702335ab75b18b02bba513853506fc76
Victor <victorfariasbezerra12@gmail.com>
2026-01-27 22:32:50 -0300

    Navbar Home URL

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('store/', include("store.urls"))
 
 ]
